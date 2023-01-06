9d15e2da23361f6f0e7168469358d3d064cf16ce
Алексей Зорин <zorinalexey59292@gmail.com>
2023-01-06 09:45:45 +0400

    final version 1.0.8

---
+++ src/defines.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! defined('SEP')) {
+    define('SEP', DIRECTORY_SEPARATOR);
+}
+
+if ( ! defined('TMP')) {
+    define('ROOT', dirname(__DIR__, 4) . SEP);
+}
+
+if ( ! defined('TMP')) {
+    define('TMP', ROOT . 'TMP' . SEP);
+}
